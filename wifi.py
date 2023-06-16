42c6d0a6813838de56c1e53ca7334cb6090f87e2
Bobo1111111 <boyuanspace@gmail.com>
2023-06-16 10:46:26 +0800

    fix

--- wifi.py
+++ wifi.py
@@ -1,7 +1,21 @@
 import urllib.request
 import time
+a=[]
+b=[]
+send = ''
+for i in a:
+    send += str(round(i, 3))
+    send += 'a'
+
+send += 'b'
+
+for i in b:
+    send += b
+    send += 'a'
+
 while True:
     urllib.request.urlopen(f'http://192.168.130.253/H')
     time.sleep(0.1)
     urllib.request.urlopen(r'http://192.168.130.253/L')
     time.sleep(0.05)
+    
